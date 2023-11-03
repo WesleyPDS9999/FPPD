1767a0938e689c62ada5034d8a38c44d23536b59
WesleyPDS9999 <wesley1234silva20@gmail.com>
2023-11-03 15:02:13 -0300

    Interface com o usuário

--- AgenciaClient.java
+++ AgenciaClient.java
@@ -1,21 +1,96 @@
 import java.rmi.Naming;
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class AgenciaClient {
 	public static void main (String[] args) {
 		double saldo;
+		Scanner sc = new Scanner(System.in);
 
-		if	(args.length != 2)  {
-			System.out.println("Uso: java AgenciaClient <maquina> <nome>");
+		if	(args.length != 1)  {
+			System.out.println("Uso: java AgenciaClient <maquina>");
 			System.exit(1);
 		}
-		try {
+		try {//criar interface com o usuario, para acessar os outros metodos
 			AgenciaInterface conta = (AgenciaInterface) Naming.lookup ("//"+args[0]+"/Agencias");
-			saldo = conta.consultarSaldo(args[1]);
-			System.out.println ("Nome: "+ args[1]);
-			if (saldo<-1.0)
-				System.out.println ("Resultado: nome nao encontrado!\n");
-			else
-				System.out.println ("Saldo: " + saldo);
+			while (true)    {
+				System.out.println("\n----------");
+				System.out.println("-- MENU --");
+				System.out.println("----------\n");
+				System.out.println("1. Abrir uma nova conta.");
+				System.out.println("2. Autenticar.");
+				System.out.println("3. Consultar seu saldo.");
+				System.out.println("4. Sacar.");
+				System.out.println("5. Depositar.");
+				System.out.println("6. Fechar conta.");
+				System.out.println("7. Finalizar o programa.");
+				System.out.println("Escolha sua opcao.");
+				try {
+					int op = sc.nextInt();
+					switch (op) {
+						case 1:
+							System.out.println("\nInforme seu nome:");
+							String nome = sc.next();
+							System.out.println("\nInforme um número de identificação para sua nova conta:");
+							int nroConta = sc.nextInt();
+							conta.solicitaAbrirConta(nome,nroConta);
+							System.out.println("\nConta aberta com sucesso.");
+							break;
+						case 2:
+							System.out.println("\nInforme seu nome:");
+							String n = sc.next();
+							System.out.println("\nInforme um número de identificação para sua nova conta:");
+							int nConta = sc.nextInt();
+							if(conta.autenticarConta(n, nConta)){
+								System.out.println("\nConta autenticada com sucesso.");
+							}else{
+								System.out.println("\nConta inválida.");
+							}
+							break;
+						case 3:
+							System.out.println("\nInforme seu nome:");
+							String n2 = sc.next();
+							System.out.println("\nSeu saldo é de: R$" + conta.consultarSaldo(n2));
+							break;
+						case 4:
+							System.out.println("\nInforme seu nome:");
+							String n3 = sc.next();
+							System.out.println("\nInforme o valor desejado para o saque:");
+							double valor = sc.nextDouble();
+							if(conta.consultarSaldo(n3) > valor){
+								conta.sacar(n3, valor);
+							}else{
+								System.out.println("Saldo insuficiente.");
+							}
+							break;
+						case 5:
+							System.out.println("\nInforme seu nome:");
+							String n4 = sc.next();
+							System.out.println("\nInforme o valor desejado para o deposito:");
+							double valor1 = sc.nextDouble();
+							conta.depositar(n4, valor1);
+							break;
+						case 6:
+							System.out.println("\nInforme seu nome:");
+							String n5 = sc.next();
+							conta.solicitaFecharConta(n5);
+							System.out.println("\nConta encerrada com sucesso.");
+							break;
+						case 7:
+							sc.close();
+							System.out.println("Finalizando Programa....");
+							System.exit(0);
+							break;
+						default:
+							System.out.println("Escolha uma opção válida");
+					}
+				}
+				catch (InputMismatchException e) {
+					System.out.println("Insira uma entrada válida");
+					sc.next();
+				}
+	
+			}
 		} catch (Exception e) {
 			System.out.println ("AgenciaClient failed.");
 			e.printStackTrace();
