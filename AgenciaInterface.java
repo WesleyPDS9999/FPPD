32cb02298b74a11d29f4c8c11a4b50241883831a
WesleyPDS9999 <wesley1234silva20@gmail.com>
2023-11-03 15:05:30 -0300

    Mudança de parâmetros

--- AgenciaInterface.java
+++ AgenciaInterface.java
@@ -4,7 +4,7 @@ import java.rmi.RemoteException;
 public interface AgenciaInterface extends Remote {
     boolean solicitaAbrirConta(String nome, int nroConta) throws RemoteException;
     boolean solicitaFecharConta(String nome) throws RemoteException;
-    boolean autenticarConta(Conta conta) throws RemoteException;
+    boolean autenticarConta(String nome, int nroConta) throws RemoteException;
     void depositar(String nome, double valor) throws RemoteException;
     void sacar(String nome, double valor) throws RemoteException;
     double consultarSaldo(String nome) throws RemoteException;
